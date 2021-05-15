39dab368ee5b9b542c074627e7d6ab08ef5bc764
2019211001000724-Xuzhihao <1449123435@qq.com>
2021-05-15 18:37:15 +0800

    10.HE Week10 Commit

---
+++ src/main/java/com/Xuzhihao/dao/ProductDao.java
@@ -0,0 +1,80 @@
+package com.Xuzhihao.dao;
+
+import com.Xuzhihao.model.Product;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProductDao implements  IProductDao{
+    @Override
+    public int save(Product product, Connection con) throws SQLException {
+        int n = 0;
+        String sql = "insert into product(ProductName,ProductDescription,Picture,price,CategoryID) values(?,?,?,?,?)";
+        PreparedStatement pt = con.prepareStatement(sql);
+        pt.setString(1, product.getProductName());
+        pt.setString(2, product.getProductDescription());
+            //for sql server
+            pt.setBinaryStream(3, product.getPicture());
+            //for mysql
+            //   pt.setBlob(3, product.getPicture());
+        pt.setDouble(4, product.getPrice());
+        pt.setInt(5, product.getCategoryId());
+        n = pt.executeUpdate();
+        if (n > 0) {
+            return n;
+        }
+        return 0;
+    }//end save
+
+    @Override
+    public int delete(Integer productId, Connection con) throws  SQLException{
+        int n = 0;
+        String sql = "delete from product where productId=?";
+        PreparedStatement pt = con.prepareStatement(sql);
+        pt.setInt(1, productId.intValue());
+        n = pt.executeUpdate();
+        if (n > 0) {
+            return n;
+        }
+        return 0;
+    }
+
+    @Override
+    public int update(Product instance, Connection con) {
+        return 0;
+    }
+
+    @Override
+    public Product findById(Integer productId, Connection con) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findByCategoryId(int categoryId, Connection con) {
+        return null;
+    }
+
+    @Override
+    public List<Product> findByPrice(double minPrice, double maxPrice, Connection con) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Product> findAll(Connection con) throws SQLException {
+
+        return null;
+    }
+
+    @Override
+    public List<Product> findByProductName(String productName, Connection con) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Product> getPicture(Integer productId, Connection con) throws SQLException {
+        return null;
+    }
+}
